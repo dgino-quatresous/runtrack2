c71725d233146092670b924fae17c09532776ec8
dgino <dgino.quatresous@laplateforme.io>
2025-09-16 13:07:20 +0200

    teletravail03

--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -3,11 +3,11 @@
 //Ce premier echo va afficher les mots "Hello LaPlateforme" qui signifie "Bonjour LaPlateforme" en anglais,
 //il s'agit d'un jeu de mot avec "Hello World" ("Bonjour Monde" en anglais) où le mot "World" est remplacer par "LaPlateforme"
 
-echo "Hello LaPlateforme"
+echo "Hello LaPlateforme";
 
 //Ce dexième echo fait exactement la même chose que le premier et n'a aucune différence apart le commentaire associé que
 //vous lisez actuelement.
 
-echo "Hello LaPlateforme"
+echo "Hello LaPlateforme";
 
 ?>
--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -4,4 +4,9 @@ $bool = true;
 $str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, delectus!";
 $val = 3.14;
 
+echo $bool;
+echo $str;
+echo $val;
+
+
 ?>
