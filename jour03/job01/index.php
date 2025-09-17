ab5116bedf1f57a6a5b1a5baacdd3ed973b8ce57
dgino <dgino.quatresous@laplateforme.io>
2025-09-17 16:08:53 +0200

    php3

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -1,14 +1,14 @@
 <?php
 
 for ($i = 0; $i <= 100; $i++) {
-        if ($i % 3 && $i % 5) {
-        echo "FizzBuzz<br>";
-        }
-        elseif ($i % 3) {
+        if ($i == 3*$i) {
         echo "Fizz<br>";
         }
-        elseif ($i % 5) {
+        elseif ($i == 5*$i) {
         echo "Buzz<br>";
+        }
+        elseif ($i == 3*$i && 5*$i) {
+        echo "FizzBuzz<br>";
         }else{
         echo "$i<br>";
     }
