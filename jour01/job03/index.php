48fd7f8a1e89c79efc249d8a5984d7272a61e5ed
dgino <dgino.quatresous@laplateforme.io>
2025-09-17 10:49:03 +0200

    php1

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -4,9 +4,8 @@ $bool = true;
 $str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, delectus!";
 $val = 3.14;
 
-echo $bool;
-echo $str;
-echo $val;
-
+echo "<h1><u>$bool</u></h1>";
+echo "<h1><b>$str</b></h1>";
+echo "<h1><i>$val</i></h1>";
 
 ?>
