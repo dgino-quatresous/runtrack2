c89a7035a2d6990b873d46788147d2cce02b10e3
dgino-quatresous <dgino.quatresous@laplateforme.io>
2026-09-07 16:25:29 +0200

    jour06

---
+++ jour06/job04/jour04/job05/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="user">username</label>
+        <input type="text" name="user" id="user">
+        <label for="pass">password</label>
+        <input type="text" name="pass" id="pass">
+        <input type="submit">
+    </form>
+</body>
+</html>
+
+
+<?php
+    if ($_POST["user"] == "John" && $_POST["pass"] == )
+?>
